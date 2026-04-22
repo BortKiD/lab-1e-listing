1069fab46cfb1569567b7d1310a8d066edfbfe4d
BortKiD <gfol_dfgcv@outlook.com>
2026-04-23 00:50:54 +0300

    First task: additional attributes and getters for them.

--- domain/Book.cpp
+++ domain/Book.cpp
@@ -1,4 +1,28 @@
 #include "Book.h"
 
-Book::Book(std::string title, std::string isbn) 
-: title_(std::move(title)), isbn_(std::move(isbn)) {}
+Book::Book(std::string title, std::string isbn,
+        std::vector<std::string> authors, int publicationYear) 
+: title_(std::move(title)), 
+isbn_(std::move(isbn)),
+authors_(std::move(authors)),
+publicationYear_(publicationYear) {}
+
+const std::string& Book::getTitle() const 
+{
+    return title_;
+}
+
+const std::string& Book::getIsbn() const 
+{
+    return isbn_;
+}
+
+const std::vector<std::string>& Book::getAuthors() const 
+{
+    return authors_;
+}
+
+int Book::getPublicationYear() const 
+{
+    return publicationYear_;
+}
