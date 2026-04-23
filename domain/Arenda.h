5b172da9698770f42dd6f864f24c7b5998752351
BortKiD <30574689+BortKiD@users.noreply.github.com>
2026-04-23 16:50:39 +0300

    11 task: arenda class changes.

--- domain/Arenda.h
+++ domain/Arenda.h
@@ -1,15 +1,19 @@
 #pragma once
 #include <memory>
+#include <chrono>
 
 class Person;
 class Copy;
 class Arenda 
 {
 public:
-    Arenda(std::shared_ptr<Person> person, std::shared_ptr<Copy> copy);
-    bool isActive();
+    Arenda(std::shared_ptr<Person> person,
+           std::shared_ptr<Copy> copy);
+    std::shared_ptr<Person> getPerson() const;
+    std::shared_ptr<Copy> getCopy() const;
+
 private:
     std::shared_ptr<Person> person_;
     std::shared_ptr<Copy> copy_;
-    bool active_ = true;
+    std::chrono::system_clock::time_point startDate_;
 };
